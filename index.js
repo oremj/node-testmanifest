82abda5a59331147e75b8056723a54e106ac1561
Jeremiah Orem <oremj@oremj.com>
2014-08-13 17:45:36 -0700

    add edit.ejs

--- index.js
+++ index.js
@@ -2,18 +2,28 @@ var express = require('express');
 var logger = require('morgan');
 var path = require('path');
 var program = require('commander');
+var uuid = require('node-uuid');
+
 var app = express();
 
 function subdomainMiddle(req, res, next) {
     
     var hostParts = req.hostname.split(".");
     if(hostParts.length < 3 || hostParts[0] == "www") {
-        next();
-        return;
+        res.locals.subdomain = uuid.v1();
+    } else {
+        req.url = "/subdomain/" + hostParts[0] + req.url;
+        res.locals.subdomain = hostParts[0];
     }
 
-    req.url = "/subdomain/" + hostParts[0] + request.url;
+    res.locals.subdomainBase = res.locals.subdomain.split('-')[0];
+    res.locals.domain = "http://" + res.locals.subdomain + "." + hostnameRoot(req.hostname);
+    res.locals.manifest = res.locals.domain + "/manifest.webapp";
     next();
+}
+
+function hostnameRoot(hostname) {
+    return hostname.split('.').slice(-2).join('.');
 }
 
 // view engine setup
@@ -24,6 +34,11 @@ app.use(logger('dev'));
 app.use(subdomainMiddle);
 app.get("/", function(req, res) {
     res.render('index');
+});
+
+app.get("/subdomain/:subdomain", function(req, res) {
+    res.locals.json = "{}";
+    res.render('edit');
 });
 
 program
